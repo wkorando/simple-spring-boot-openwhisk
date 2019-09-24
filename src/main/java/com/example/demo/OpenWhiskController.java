20023bbdf107e989dea23173dc63fe0ab537281e
prpatel <pratik@mypatelspace.com>
2019-09-24 16:14:55 -0400

    added project files

---
+++ src/main/java/com/example/demo/OpenWhiskController.java
@@ -0,0 +1,32 @@
+package com.example.demo;
+
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+public class OpenWhiskController {
+    @RequestMapping(value = "/init", method = RequestMethod.POST)
+
+    public ResponseEntity<Map> init(@RequestBody Map params) {
+
+        HashMap<String, Object> map = new HashMap<>();
+        map.put("OK", true);
+
+        return new ResponseEntity(map, HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/run", method = RequestMethod.POST)
+    public ResponseEntity< Map > run(@RequestBody Map params) {
+        HashMap<String, Object> map = new HashMap<>();
+        map.put("name", params.get("name"));
+        return new ResponseEntity<Map>(map, HttpStatus.OK);
+    }
+}
