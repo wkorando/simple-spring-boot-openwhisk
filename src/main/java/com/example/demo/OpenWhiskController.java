09c25f5277455a2ca38081d2aa90637db35103b2
Billy Korando <wkorando@gmail.com>
2020-05-14 17:01:21 -0500

    Updating simplifying stuff

--- src/main/java/com/example/demo/OpenWhiskController.java
+++ src/main/java/com/example/demo/OpenWhiskController.java
@@ -1,33 +1,37 @@
 package com.example.demo;
 
 
-import org.springframework.http.HttpStatus;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.HashMap;
-import java.util.Map;
+import com.google.gson.JsonObject;
 
 @RestController
-@RequestMapping("/api")
 public class OpenWhiskController {
 	
     @RequestMapping(value = "/init", method = RequestMethod.POST)
-    public ResponseEntity<Map> init(@RequestBody Map params) {
+    public ResponseEntity<?> init(@RequestBody JsonObject args) {
+    	System.out.println("In init");
+    	System.out.println(args.toString());
 
-        HashMap<String, Object> map = new HashMap<>();
-        map.put("OK", true);
-
-        return new ResponseEntity(map, HttpStatus.OK);
+        return ResponseEntity.ok().build();
     }
 
-    @RequestMapping(value = "/run", method = RequestMethod.POST)
-    public ResponseEntity< Map > run(@RequestBody Map params) {
-        HashMap<String, Object> map = new HashMap<>();
-        map.put("name", params.get("name"));
-        return new ResponseEntity<Map>(map, HttpStatus.OK);
+    @PostMapping(value = "/run", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Map<Object, Object>> run(@RequestBody Map<Object, Object> args) {
+    	System.out.println("In run");
+    	System.out.println(args.toString());
+    	Map<Object, Object> response = new   HashMap<>();
+        String result = "Hello! Welcome to OpenWhisk...";
+        response.put("message", result);
+        return ResponseEntity.ok().body(response);
     }
 }
